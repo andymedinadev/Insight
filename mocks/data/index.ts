d232a4dd921617b9a8b8f506ee40367ac39fa2b1
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-11-10 17:21:17 -0300

    feat(mocks): create mock data modules

---
+++ mocks/data/index.ts
@@ -0,0 +1,4 @@
+export { initialMaterials } from './materials';
+export { initialNotes } from './notes';
+export { initialArchivedPatients, initialPatients } from './patients';
+export { demoUser } from './user';
